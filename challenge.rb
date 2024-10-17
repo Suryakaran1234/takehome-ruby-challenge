b19592867cbe6fa7c58a29c962ae859f0e3d941f
Surya Karan Sharma <Suryakaran1234@gmail.com>
2024-10-16 23:10:05 -0400

    Fixed Some Spacing in Output.txt

--- challenge.rb
+++ challenge.rb
@@ -1,6 +1,6 @@
 require 'json'
 
-# Loding and Parsing Files : users.json and companies.json
+# Loading and Parsing Files : users.json and companies.json
 def load_json(filename)
   JSON.parse(File.read(filename), symbolize_names: true)
 rescue StandardError => e
@@ -14,17 +14,20 @@ def process_data(users_file, companies_file, output_file)
   companies = load_json(companies_file)
 
   File.open(output_file, 'w') do |file|
+    # Adds Empty line on Top of output.txt file
+    file.puts
+
     # Sorting companies by id
     sorted_companies = companies.sort_by { |company| company[:id] }
 
     sorted_companies.each do |company|
-      file.puts "Company Id: #{company[:id]}"
-      file.puts "Company Name: #{company[:name]}"
+      file.puts "\tCompany Id: #{company[:id]}"
+      file.puts "\tCompany Name: #{company[:name]}"
       
-      # Filter users by active status and belonging to this company
+      # Filtering out users by active status and who belongs to this company
       company_users = users.select { |user| user[:company_id] == company[:id] && user[:active_status] }
       
-      # Sort users by last name alphabetically
+      # Sorting users by last name alphabetically
       sorted_users = company_users.sort_by { |user| user[:last_name] }
 
       total_top_up = 0
@@ -45,7 +48,7 @@ def process_data(users_file, companies_file, output_file)
                          'Not emailed'
                        end
 
-        # Collect users to appropriate sections
+        # Separating users who should receive email and who should not
         if user[:email_status] && company[:email_status]
           users_emailed << user_info(user, previous_tokens, new_tokens)
         else
@@ -54,19 +57,19 @@ def process_data(users_file, companies_file, output_file)
       end
 
       # users who were emailed
-      file.puts "Users Emailed:"
+      file.puts "\tUsers Emailed:"
       if not users_emailed.empty?
         users_emailed.each { |info| file.puts info }
       end
 
       # users who were not emailed
-      file.puts "Users Not Emailed:"
+      file.puts "\tUsers Not Emailed:"
       if not users_not_emailed.empty?
         users_not_emailed.each { |info| file.puts info }
       end
 
       # Print total top-ups for the company
-      file.puts "Total amount of top ups for #{company[:name]}: #{total_top_up}"
+      file.puts "\t \tTotal amount of top ups for #{company[:name]}: #{total_top_up}"
       file.puts # Blank line after each company section
     end
   end
@@ -75,9 +78,9 @@ end
 # function to format users in output.txt
 def user_info(user, previous_tokens, new_tokens)
   <<~INFO
-    \t#{user[:last_name]}, #{user[:first_name]}, #{user[:email]}
-    \t  Previous Token Balance: #{previous_tokens}
-    \t  New Token Balance: #{new_tokens}
+    \t \t#{user[:last_name]}, #{user[:first_name]}, #{user[:email]}
+    \t \t \tPrevious Token Balance: #{previous_tokens}
+    \t \t \tNew Token Balance: #{new_tokens}
   INFO
 end
 
